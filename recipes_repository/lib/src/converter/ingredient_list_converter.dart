6a391703cb7d2eefe5cd942ad873e822a65afbcb
niclas Prock <niclasprock@gmail.com>
2021-07-18 11:59:51 +0200

    FEATURE: error handling for ingredient list converter

--- recipes_repository/lib/src/converter/ingredient_list_converter.dart
+++ recipes_repository/lib/src/converter/ingredient_list_converter.dart
@@ -1,6 +1,8 @@
+import 'dart:convert' as convert;
+
 import 'package:json_annotation/json_annotation.dart';
 import 'package:recipes_repository/recipes_repository.dart';
-import 'dart:convert' as convert;
+import 'package:recipes_repository/src/failures/ingredient_list_converter_failure.dart';
 
 class CustomIngredientListConverter
     implements JsonConverter<IngredientList, List<dynamic>> {
@@ -8,20 +10,27 @@ class CustomIngredientListConverter
 
   @override
   IngredientList fromJson(List<dynamic> json) {
-    return IngredientList(json.map((e) {
-      final x = convert.json.decode(e);
-      return Ingredient.fromJson(x);
+    if (json.whereType<String>().length < json.length)
+      throw IngredientListConverterFailure.badJsonType(json.runtimeType);
+    return IngredientList(json.map((ingredientJson) {
+      try {
+        final decodedJson = convert.json.decode(ingredientJson);
+        return Ingredient.fromJson(decodedJson);
+      } on FormatException catch (e) {
+        throw IngredientListConverterFailure.decodeFailure(ingredientJson, e);
+      } catch (e) {
+        throw IngredientListConverterFailure.unknownError(e);
+      }
     }).toList());
   }
 
   @override
-  List<dynamic> toJson(IngredientList json) {
-    if (json.ingredients != null) {
-      return json.ingredients!
-          .map<String>((e) => convert.json.encode(e.toJson()))
-          .toList();
-    } else {
-      throw Error();
-    }
+  List<dynamic> toJson(IngredientList ingredientList) {
+    if (ingredientList.ingredients == null)
+      throw IngredientListConverterFailure.ingredientListInvalid(
+          ingredientList);
+    return ingredientList.ingredients!
+        .map<String>((e) => convert.json.encode(e.toJson()))
+        .toList();
   }
 }
---
+++ recipes_repository/lib/src/failures/ingredient_list_converter_failure.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:recipes_repository/recipes_repository.dart';
+
+part 'ingredient_list_converter_failure.freezed.dart';
+
+@freezed
+class IngredientListConverterFailure with _$IngredientListConverterFailure {
+  const factory IngredientListConverterFailure.badJsonType(Type type) = BadType;
+  const factory IngredientListConverterFailure.ingredientListInvalid(
+      IngredientList ingredientList) = IngredientListInvalid;
+  const factory IngredientListConverterFailure.decodeFailure(
+      String ingredientJson, Object? error) = DecodeFailure;
+  const factory IngredientListConverterFailure.unknownError(Object? error) =
+      UnknownError;
+}
